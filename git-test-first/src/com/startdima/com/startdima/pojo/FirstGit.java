516fc2d3138bf8243fe94e9f294d508aa7bcbcab
duhb <627936549@qq.com>
2020-07-23 00:03:12 +0800

    提交项目

---
+++ git-test-first/src/com/startdima/com/startdima/pojo/FirstGit.java
@@ -0,0 +1,12 @@
+package com.startdima.com.startdima.pojo;
+
+public class FirstGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
